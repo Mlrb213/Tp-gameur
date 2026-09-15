903ecb1bc678346c9b15f7017ebb14f4b126095d
Mohamed Larbi <altiseo.france@gmail.com>
2026-09-15 16:40:51 +0200

    push

--- app/data/skillsData.ts
+++ app/data/skillsData.ts
@@ -1,35 +1,35 @@
 import type { Rawskills } from "../utils/competence"
 
 export const skills = {
-    basicAttack:{
-        idSkills: 1, 
+    basicAttack: {
+        idSkills: 1,
         nameSkills: 'basicAttack',
         type: 'basicAttack',
         amount: 20
     },
-    SpecialAttack:{
+    SpecialAttack: {
         idSkills: 2,
         nameSkills: 'specialAttack',
         type: 'specialAttack',
         amount: 35,
         coolDown: 2
     },
-    heal:{
+    heal: {
         idSkills: 3,
         nameSkills: 'heal',
         type: 'heal',
         amount: 50,
         coolDown: 2,
-        maxCharge: 2 
+        maxCharge: 2
     },
-    shield:{
+    shield: {
         idSkills: 4,
         nameSkills: 'shield',
         type: 'shield',
         amount: 20,
         coolDown: 2
     },
-    luck:{
+    luck: {
         idSkills: 5,
         nameSkills: 'luck',
         type: 'luck',
