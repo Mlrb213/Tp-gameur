bdbbe85e9051fa05156adb034fe985f0b14613f2
toffa harryson <toffaharryson3@gmail.com>
2026-09-15 11:38:09 +0200

    ?

---
+++ app/data/monsterData.ts
@@ -0,0 +1,6 @@
+import { Monster} from "~/utils/Monster";
+import { RawMonster } from "~/utils/Monster";
+
+export const monsters : Monster[] = [
+    new Monster(0, 'Pokemon1', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png', 100, 100, 20, 10)
+]
